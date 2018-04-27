ce768b566fd0cfbf18251b87098d35bc04962ff7
Felix Lange <fjl@users.noreply.github.com>
2018-04-27 11:13:23 +0200

    core/state: cache missing storage entries

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -187,9 +187,7 @@ func (self *stateObject) GetState(db Database, key common.Hash) common.Hash {
 		}
 		value.SetBytes(content)
 	}
-	if (value != common.Hash{}) {
-		self.cachedStorage[key] = value
-	}
+	self.cachedStorage[key] = value
 	return value
 }
 
@@ -206,11 +204,6 @@ func (self *stateObject) SetState(db Database, key, value common.Hash) {
 func (self *stateObject) setState(key, value common.Hash) {
 	self.cachedStorage[key] = value
 	self.dirtyStorage[key] = value
-
-	if self.onDirty != nil {
-		self.onDirty(self.Address())
-		self.onDirty = nil
-	}
 }
 
 // updateTrie writes cached storage modifications into the object's storage trie.
