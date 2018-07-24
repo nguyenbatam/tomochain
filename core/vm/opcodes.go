40cd1c835974b8b349ff3c30d52ce1e2a99e8b6a
gary rong <garyrong0905@gmail.com>
2018-07-24 22:22:03 +0800

    core, crypto, params: implement CREATE2 evm instrction (#17196)
    * core, crypto, params: implement CREATE2 evm instrction
    
    * core/vm: add opcode to string mapping
    
    * core: remove past fork checking
    
    * core, crypto: use option2 to generate new address

--- core/vm/opcodes.go
+++ core/vm/opcodes.go
@@ -205,6 +205,7 @@ const (
 	CALLCODE
 	RETURN
 	DELEGATECALL
+	CREATE2
 	STATICCALL = 0xfa
 
 	REVERT       = 0xfd
@@ -366,6 +367,7 @@ var opCodeToString = map[OpCode]string{
 	RETURN:       "RETURN",
 	CALLCODE:     "CALLCODE",
 	DELEGATECALL: "DELEGATECALL",
+	CREATE2:      "CREATE2",
 	STATICCALL:   "STATICCALL",
 	REVERT:       "REVERT",
 	SELFDESTRUCT: "SELFDESTRUCT",
@@ -517,6 +519,7 @@ var stringToOp = map[string]OpCode{
 	"LOG3":           LOG3,
 	"LOG4":           LOG4,
 	"CREATE":         CREATE,
+	"CREATE2":        CREATE2,
 	"CALL":           CALL,
 	"RETURN":         RETURN,
 	"CALLCODE":       CALLCODE,
