e7c3971da4dd104199d1a0dc24189737ee201c26
Nguyen Ba Tam <nguyenbatam90@gmail.com>
2018-09-14 15:50:34 +0700

    Squashed commit of the following:
    commit 6c73e2c670efa670f6fdb58605793dc3acab7228
    Author: Nguyen Ba Tam <nguyenbatam90@gmail.com>
    Date:   Fri Sep 14 15:46:19 2018 +0700
    
        make up code
    
    commit 65e91c806c908c3577c4a573f25669c2f5351551
    Author: Nguyen Ba Tam <nguyenbatam90@gmail.com>
    Date:   Fri Sep 14 15:40:39 2018 +0700
    
        update number worker base on numberCPU
    
    commit 601261346e78ec3b2f25423fe61763fce46472ca
    Author: Nguyen Ba Tam <nguyenbatam90@gmail.com>
    Date:   Thu Sep 13 14:31:50 2018 +0700
    
        concurrence init messages from transactions when insert block

--- core/state_processor.go
+++ core/state_processor.go
@@ -27,8 +27,13 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 )
 import (
+<<<<<<< HEAD
 	"sync"
 	"runtime"
+=======
+	"runtime"
+	"sync"
+>>>>>>> optimize_insert_new_block
 )
 
 // StateProcessor is a basic Processor, which takes care of transitioning
@@ -69,7 +74,11 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 	if p.config.DAOForkSupport && p.config.DAOForkBlock != nil && p.config.DAOForkBlock.Cmp(block.Number()) == 0 {
 		misc.ApplyDAOHardFork(statedb)
 	}
+<<<<<<< HEAD
 	nWorker :=runtime.NumCPU()
+=======
+	nWorker := runtime.NumCPU()
+>>>>>>> optimize_insert_new_block
 	InitSignerInTransactions(p.config, header, block.Transactions(), nWorker)
 	// Iterate over and process the individual transactions
 	for i, tx := range block.Transactions() {
