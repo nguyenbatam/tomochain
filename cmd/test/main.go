4bff2b92556202dab9d8dadf8b011a87389ab750
Nguyen Ba Tam <nguyenbatam90@gmail.com>
2019-11-11 10:05:49 +0700

    update

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/eth"
 	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/ethereum/go-ethereum/rlp"
+	"log"
+	"os"
 	"runtime"
 )
 
 var (
 	from    = flag.String("from", "/data/tomo/chaindata", "directory to TomoChain chaindata")
-	to      = flag.String("to", "/data/tomo/chaindata_copy", "directory to clean chaindata")
-	root    = flag.String("root", "", "state root compare")
+	to      = flag.String("to", "/data/tomo/copy", "directory to clean chaindata")
+	root    = flag.String("root", "0xc96e205f8e0d7dccea94c04fde6e6f7e508a3bbb91d2630335b37fbe23ec3a87", "state root compare")
 	address = flag.String("address", "/data/tomo/adress.txt", "list address in state db")
 
 	sercureKey = []byte("secure-key-") // preimagePrefix + hash -> preimage
@@ -32,85 +38,84 @@ var (
 )
 
 func main() {
-	toDB, err = ethdb.NewLDBDatabase("/data/tomo/copy", eth.DefaultConfig.DatabaseCache, utils.MakeDatabaseHandles())
+	flag.Parse()
+	fmt.Println("flag")
+	fromDB, err = ethdb.NewLDBDatabase(*from, eth.DefaultConfig.DatabaseCache, utils.MakeDatabaseHandles())
+	defer fromDB.Close()
+	if err != nil {
+		fmt.Println("fromDB", err)
+		return
+	}
+	fmt.Println(fromDB)
+	toDB, err = ethdb.NewLDBDatabase(*to, eth.DefaultConfig.DatabaseCache, utils.MakeDatabaseHandles())
 	defer toDB.Close()
-	//core.WriteCanonicalHash(toDB,common.HexToHash("0x9326145f8a2c8c00bbe13afc7d7f3d9c868b5ef39d89f2f4e9390e9720298624"),0)
-	genesiHash := core.GetCanonicalHash(toDB, 0)
-	genesisBlock := core.GetBlock(toDB, genesiHash, 0)
-	genesisTd := core.GetTd(toDB, genesiHash, 0)
-	fmt.Println("genesiHash",genesiHash)
-	fmt.Println("genesisBlock",genesisBlock)
-	fmt.Println("genesisTd",genesisTd)
-	//flag.Parse()
-	//fromDB, err = ethdb.NewLDBDatabase(*from, eth.DefaultConfig.DatabaseCache, utils.MakeDatabaseHandles())
-	//defer fromDB.Close()
-	//if err != nil {
-	//	fmt.Println("fromDB", err)
-	//	return
-	//}
-	//toDB, err = ethdb.NewLDBDatabase(*to, eth.DefaultConfig.DatabaseCache, utils.MakeDatabaseHandles())
-	//defer toDB.Close()
-	//if err != nil {
-	//	fmt.Println("toDB", err)
-	//	return
-	//}
-	//fromBC, err := core.NewBlockChain(fromDB, nil, nil, nil, vm.Config{})
-	//if err != nil {
-	//	fmt.Println("fromBC", err)
-	//	return
-	//}
-	//toBC, err := core.NewBlockChain(toDB, nil, nil, nil, vm.Config{})
-	//if err != nil {
-	//	fmt.Println("toBC", err)
-	//	return
-	//}
-	//fromState, err := fromBC.StateAt(common.HexToHash(*root))
-	//if err != nil {
-	//	fmt.Println("fromState", err)
-	//	return
-	//}
-	//toState, err := toBC.StateAt(common.HexToHash(*root))
-	//if err != nil {
-	//	fmt.Println("toState", err)
-	//	return
-	//}
-	//
-	//f, err := os.Open(*address)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//defer f.Close()
-	//
-	//scanner := bufio.NewScanner(f)
-	//for scanner.Scan() {
-	//	addr := common.HexToAddress(scanner.Text())
-	//	objectFrom := fromState.GetOrNewStateObject(addr)
-	//	byteFrom, err := rlp.EncodeToBytes(objectFrom)
-	//	if err != nil {
-	//		fmt.Println("objectFrom", err)
-	//	}
-	//
-	//	objectTo := toState.GetOrNewStateObject(addr)
-	//	byteTo, err := rlp.EncodeToBytes(objectTo)
-	//	if err != nil {
-	//		fmt.Println("objectTo", err)
-	//	}
-	//
-	//	if bytes.Compare(byteFrom, byteTo) != 0 {
-	//		fmt.Println("Fail when compare 2 address ", addr, common.Bytes2Hex(byteFrom), common.Bytes2Hex(byteTo))
-	//		break
-	//	}
-	//	if bytes.Compare(objectFrom.CodeHash(), emptyState) != 0 {
-	//		fromState.ForEachStorage(addr, func(key, value common.Hash) bool {
-	//			toValue := toState.GetState(addr, key)
-	//			if value != toValue {
-	//				fmt.Println("Fail when compare 2 state in address ", addr, "key", key.Hex(), "fromValue", value.Hex(), "toValue", toValue.Hex())
-	//			}
-	//			return true
-	//		})
-	//	}
-	//}
-	//if err := scanner.Err(); err != nil {
-	//	log.Fatal(err)
-	//}
+	if err != nil {
+		fmt.Println("toDB", err)
+		return
+	}
+	fmt.Println(toDB)
+	fromBC, err := core.NewBlockChain(fromDB, nil, nil, nil, vm.Config{})
+	if err != nil {
+		fmt.Println("fromBC", err)
+		return
+	}
+	fmt.Println(fromBC)
+	toBC, err := core.NewBlockChain(toDB, nil, nil, nil, vm.Config{})
+	if err != nil {
+		fmt.Println("toBC", err)
+		return
+	}
+	fmt.Println(toBC)
+	fromState, err := fromBC.StateAt(common.HexToHash(*root))
+	if err != nil {
+		fmt.Println("fromState", err)
+		return
+	}
+	fmt.Println(fromState)
+	toState, err := toBC.StateAt(common.HexToHash(*root))
+	if err != nil {
+		fmt.Println("toState", err)
+		return
+	}
+	fmt.Println(toState)
+	f, err := os.Open(*address)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		addr := common.HexToAddress(scanner.Text())
+		objectFrom := fromState.GetOrNewStateObject(addr)
+		byteFrom, err := rlp.EncodeToBytes(objectFrom)
+		if err != nil {
+			fmt.Println("objectFrom", err)
+		}
+		fmt.Println("addr",addr)
+
+		objectTo := toState.GetOrNewStateObject(addr)
+		byteTo, err := rlp.EncodeToBytes(objectTo)
+		if err != nil {
+			fmt.Println("objectTo", err)
+		}
+
+		if bytes.Compare(byteFrom, byteTo) != 0 {
+			fmt.Println("Fail when compare 2 address ", addr, common.Bytes2Hex(byteFrom), common.Bytes2Hex(byteTo))
+			break
+		}
+		fmt.Println("addr",addr,"code hash ",common.Bytes2Hex(objectFrom.CodeHash()))
+		if bytes.Compare(objectFrom.CodeHash(), emptyState) != 0 {
+			fromState.ForEachStorage(addr, func(key, value common.Hash) bool {
+				toValue := toState.GetState(addr, key)
+				if value != toValue {
+					fmt.Println("Fail when compare 2 state in address ", addr, "key", key.Hex(), "fromValue", value.Hex(), "toValue", toValue.Hex())
+				}
+				return true
+			})
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
